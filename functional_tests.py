030671344421cd5dd671d51747d650929cf8fb3b
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2021-09-23 20:45:07 +0200

    additional funtional testing

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
+import time
+
 from selenium import webdriver
 import unittest
+
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,9 +19,39 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # checks if title or header of the site mention to-do lists
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name("h1").text
         # fail - makes test fail no matter what here used to show message
-        self.fail("Finish the test!")
+        self.assertIn("To-Do", header_text)
 
+        # user enters to-do item
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(inputbox.get_attribute('placeholder'), "Enter a to-do item")
+
+        # user input - position in to-do list
+        inputbox.send_keys('Buy peacock feathers')
+
+        # when user hits enter, the page updates and now there is one position on the list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # another item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # the page updates again and now show both items on the list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
+        # there is still present box which enables adding new input
+        self.fail("Finish the test!")
 
 # checks if script was run by shell not imported to another script
 if __name__ == "__main__":
--- lists/tests.py
+++ lists/tests.py
@@ -8,15 +8,19 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_respolves_to_home_page_view(self):
-        # resolve method shows what view is mapped to url
-        found = resolve('/')
-        self.assertEqual(found.func, views.home_page)
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 
-    def test_home_age_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get('/')
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
