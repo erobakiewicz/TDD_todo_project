4fe49effb73cc3f9b075950f72957b056ac4de22
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2021-09-23 18:46:33 +0200

    first functional test

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,9 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
 from lists import views
+from lists.views import home_page
 
 
 class HomePageTest(TestCase):
@@ -10,3 +12,11 @@ class HomePageTest(TestCase):
         # resolve method shows what view is mapped to url
         found = resolve('/')
         self.assertEqual(found.func, views.home_page)
+
+    def test_home_age_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
