8ed2f69fd198e03d54458a69e60da7d21cf181d7
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2021-10-12 18:26:15 +0200

    test for forms

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from selenium.webdriver.common.keys import Keys
 
 from functional_tests.base import FunctionalTest
@@ -39,6 +41,7 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
 
+    @skip
     def test_cannot_add_duplicate_items(self):
         # edith goes to the home page and starts a new list
         self.browser.get(self.live_server_url)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,9 +1,6 @@
-from unittest import skip
-
 from django.test import TestCase
-from django.utils.html import escape
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
 from lists.models import Item, List
 
 
@@ -110,15 +107,10 @@ class NewListTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_for_invalid_input_passes_form_to_template(self):
-        response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
-
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, EMPTY_ITEM_ERROR)
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -126,8 +118,7 @@ class NewListTest(TestCase):
             f'/lists/{list1.id}/',
             data={'text': 'textey'}
         )
-
-        expected_error = "You cannot have an empty list item"
+        expected_error = DUPLICATE_ITEM_ERROR
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
@@ -149,11 +140,11 @@ class NewListTest(TestCase):
         self.assertContains(response, EMPTY_ITEM_ERROR)
 
     def test_for_invalid_input_passes_form_to_template(self):
-        response = self.client.post('/lists/new', data={'text': ''})
-        self.assertIsInstance(response.context['form'], ItemForm)
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
