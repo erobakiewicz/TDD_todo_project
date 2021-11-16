2f91f0d5afc2a1a905af3013c325ecd943b6ead0
erobakiewicz <eliasz.robakiewicz@laboratorium.ee>
2021-11-16 11:24:34 +0100

    Waiter decorator finished

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -42,15 +42,9 @@ class FunctionalTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    @wait
     def wait_for(self, fn):
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
